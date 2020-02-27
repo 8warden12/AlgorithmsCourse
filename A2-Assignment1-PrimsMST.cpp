c5223c2f0fdb22c23ea422f4416451f3b9389fe2
8warden12 <44561881+8warden12@users.noreply.github.com>
2020-02-27 08:36:00 -0800

    Update A2-Assignment1-PrimsMST.cpp

--- A2-Assignment1-PrimsMST.cpp
+++ A2-Assignment1-PrimsMST.cpp
@@ -9,7 +9,7 @@ using namespace std;
 struct node;
 
 struct edge{
-  int weight = 0;
+  int cost = 0;
   node* a = NULL;
   node* b = NULL;
 };
@@ -18,10 +18,25 @@ struct node{
   vector<edge*> connections;
 };
 
+vector<node*> nodes;
+
 int main()
 {
   fstream f("test.txt");
-  int nodes, edges;
-  f>>nodes>>edges;
+  int NODES, EDGES;
+  f>>NODES>>EDGES;
+  for(int i = 0; i<NODES; ++i)
+  {
+    nodes.push_back(new node);
+  } 
+  int n1,n2,cost;
+  for(int i = 0; i<EDGES; ++i)
+  {
+    f>>n1>>n2>>cost;
+    edge* e = new edge;
+    e->a = nodes[n1-1];
+    e->b = nodes[n2-1];
+    e->cost = cost;
+  }
 
 }
